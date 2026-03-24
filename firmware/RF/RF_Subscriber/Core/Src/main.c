92a0389724bbc5e499f54bb2d5f731d5ebbe6f6c
Austin Howell <ahowellengineering@gmail.com>
2026-03-24 18:46:27 -0400

    this just isn't working

--- firmware/RF/RF_Subscriber/Core/Src/main.c
+++ firmware/RF/RF_Subscriber/Core/Src/main.c
@@ -49,6 +49,8 @@ UART_HandleTypeDef huart1;
 /* USER CODE BEGIN PV */
 volatile uint8_t receivedCounter = 0;
 volatile uint8_t rssiValue = 0;
+uint8_t version = 0;
+uint8_t partnum = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -104,6 +106,8 @@ int main(void)
   // Reset length for each new packet
   uint8_t rxBuffer[64];
   uint8_t rxLength = 64;
+  version = TI_read_status(CCxxx0_VERSION);
+  partnum = TI_read_status(CCxxx0_PARTNUM);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -191,7 +195,7 @@ static void MX_SPI1_Init(void)
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
